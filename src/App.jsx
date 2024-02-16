06d2149a243ff3e4a21c2e7176a5067be9f14c49
Liviu Cauia <Liviu.Cauia@hrp.org.uk>
2024-02-16 15:17:32 +0000

    Add full path to power bi web version

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Iframe from './components/Iframe';
 
 const App = () => {
   const [iframeUrl, setIframeUrl] = useState(buttonData[0].buttons[0].src);
-  const [path, setFullPath] = useState(buttonData[1].buttons[0].src);
+  const [path, setFullPath] = useState(buttonData[0].buttons[0].fullPath);
 
   const handleButtonClick = (url, itemPath) => {
     setIframeUrl(url);
@@ -18,18 +18,19 @@ const App = () => {
       <div className="report-banner">IS Ops PowerBI Dashboard</div>
 
       <div className="flex justify-around w-full">
-        <div className="w-[12%]">
-          <a
-            className="block text-center mt-4 p-1 border-none bg-[#6c757d] text-white text-[14px] cursor-pointer rounded hover:bg-[#5a6268]"
-            href={path}
-            target="_blank"
-          >
-            Open Full Version
-          </a>
+        <div className="w-[12%] flex flex-col justify-between">
+         
           <ButtonGroup
             buttonData={buttonData}
             onItemClick={handleButtonClick}
           />
+           <a
+            className="block text-center mb-20 p-1 border-none bg-[#6c757d] text-white text-[14px] cursor-pointer rounded hover:bg-[#5a6268]"
+            href={path}
+            target="_blank"
+          >
+            Open Web Version
+          </a>
         </div>
         <div className="w-[80%]">
           <Iframe src={iframeUrl} />
